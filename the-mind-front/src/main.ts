9b449af1cccc7843b81540070a4b3bf288309cbf
Yoann Michon <62294558+Yoann-Michon@users.noreply.github.com>
2023-12-14 11:56:18 +0100

    front (#2)

--- the-mind-front/src/main.ts
+++ the-mind-front/src/main.ts
@@ -6,25 +6,27 @@ socket.on("connect", () => {
   console.log("connected");
 });
 
-socket.on("disconnect", () => {
-  console.log("disconnected");
-});
-
+const game = document.getElementById('game') as HTMLElement;
 const form = document.getElementById('form') as HTMLFormElement;
 const input = document.getElementById('input') as HTMLInputElement;
-const messages = document.getElementById('messages') as HTMLUListElement;
+const option = document.getElementById('options');
+const life = document.createElement('div')
+      life.setAttribute('id', 'life');
+      
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
-  }
-});
+form.addEventListener('submit', (e)=>{e.preventDefault()});
 
-socket.on('fghfhgfh', (msg) => {
-  const item = document.createElement('li');
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+const buttonPlay = document.getElementById('play') as HTMLButtonElement;
+      buttonPlay.addEventListener('click',()=>{
+        socket.emit('start game', input.value);
+        form.style.display='none';
+      });
+
+const buttonPlayer = document.getElementById('btn-player1') as HTMLButtonElement;
+      buttonPlayer.textContent= 'Ready';
+socket.emit()
+
+
+socket.on("disconnect", () => {
+  console.log("disconnected");
 });
